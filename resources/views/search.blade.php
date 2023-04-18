c9d4d4bc47efa9b6dc010a716b6216b82fa3005d
Scott Metoyer <scott.metoyer@gmail.com>
2023-04-17 17:07:02 -0700

    Created search stylesheet and search block templates

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,15 +1,16 @@
-<div x-data="{
-    source: 'https://library-public-api-f7ju7.ondigitalocean.app/api/search?q='
+<div class="search-block" x-data="{
+    source: 'Calisphere',
     query: new URLSearchParams(location.search).get('q'),
-    results: null,
-
-    search() {
-      fetch(this.source + this.query)
-        .then(response => response.json())
-        .then(data => this.results = data)
-    }
-  }" x-init="search()">
+    results: [],
+    count: 0,
+  }" x-init=" fetch('https://library-public-api-f7ju7.ondigitalocean.app/api/search?q=' + query + '&s=' + source)
+  .then(res => res.json())
+  .then(res => {
+    results = res.results;
+    count = res.total;
+  })">
+  <h4 class="search-source-title">Result count: <span x-text="count">&nbsp;</span></h4>
   <template x-for="result in results">
-    <p x-text="results.title"></p>
+    <a class="search-link" x-text="result.title" :href="result.url">&nbsp;</a>
   </template>
 </div>
