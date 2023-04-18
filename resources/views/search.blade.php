dee9edb07f6044fd4e5356afad6866d82728313a
Scott Metoyer <scott.metoyer@gmail.com>
2023-04-18 12:22:32 -0700

    Added loading indicator to search box

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -3,21 +3,27 @@
     query: new URLSearchParams(location.search).get('q'),
     results: [],
     count: 0,
-    all_results_url: ''
+    all_results_url: '',
+    loading: true
   }" x-init=" fetch('https://library-public-api-f7ju7.ondigitalocean.app/api/search?q=' + query + '&s=' + source)
   .then(res => res.json())
   .then(res => {
     results = res.results;
     count = res.total;
     all_results_url = res.all_results_url;
+    loading = false;
   })">
   <div class="search-source-header">
     <div class="source-title" x-text="source">&nbsp</div>
-    <a class="all-results-link" :href="all_results_url">See all <span x-text="count">&nbsp;</span> results</a>
+    <a class="all-results-link" :href="all_results_url" x-show="count > 0">See all <span x-text="count">&nbsp;</span> results</a>
   </div>
   <div class="search-results">
     <template x-for="result in results">
       <a class="search-link" x-text="result.title" :href="result.url">&nbsp;</a>
     </template>
+
+    <div x-show="loading">
+      Loading...
+    </div>
   </div>
 </div>
