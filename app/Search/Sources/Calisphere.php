a3dd9b1ca4f1b81807fe8b74a2ed51f5b3aba33e
Scott Metoyer <scott.metoyer@gmail.com>
2023-04-18 14:41:01 -0700

    Got Calisphere search working

--- app/Search/Sources/Calisphere.php
+++ app/Search/Sources/Calisphere.php
@@ -3,6 +3,7 @@
 namespace App\Search\Sources;
 
 use App\Search\SearchSourceInterface;
+use Exception;
 
 class Calisphere implements SearchSourceInterface
 {
@@ -15,38 +16,45 @@ class Calisphere implements SearchSourceInterface
 
   public function results()
   {
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($curl, CURLOPT_URL, "https://solr.calisphere.org/solr/query/?q=" . urlencode($this->query) . "&rows=5&wt=json&indent=true&mm=100%25&pf3=title&pf=text,title&qs=12&ps=12");
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array('X-Authentication-Token: 82dbd622-32c4-4169-b25f-5435ef337a93'));
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    $res = curl_exec($curl);
+
     $results = [];
-    $results[] = [
-      'title' => 'Calisphere',
-      'url' => 'https://calisphere.org/search/?q=' . urlencode($this->query),
-      'description' => 'Calisphere is a gateway to digital collections from California\'s great libraries, archives, and museums. Discover over 1,000,000 images, texts, and recordings—and counting.',
-      'image' => 'https://calisphere.org/images/logo.png'
-    ];
-    $results[] = [
-      'title' => 'Calisphere 2',
-      'url' => 'https://calisphere.org/search/?q=' . urlencode($this->query),
-      'description' => 'Calisphere is a gateway to digital collections from California\'s great libraries, archives, and museums. Discover over 1,000,000 images, texts, and recordings—and counting.',
-      'image' => 'https://calisphere.org/images/logo.png',
-    ];
-    $results[] = [
-      'title' => 'Calisphere 34',
-      'url' => 'https://calisphere.org/search/?q=' . urlencode($this->query),
-      'description' => 'Calisphere is a gateway to digital collections from California\'s great libraries, archives, and museums. Discover over 1,000,000 images, texts, and recordings—and counting.',
-      'image' => 'https://calisphere.org/images/logo.png',
-    ];
-    $results[] = [
-      'title' => 'Calisphere 55',
-      'url' => 'https://calisphere.org/search/?q=' . urlencode($this->query),
-      'description' => 'Calisphere is a gateway to digital collections from California\'s great libraries, archives, and museums. Discover over 1,000,000 images, texts, and recordings—and counting.',
-      'image' => 'https://calisphere.org/images/logo.png',
-    ];
+
+    if ($res === false) {
+      $res = curl_error($curl);
+      curl_close($curl);
+      throw new Exeption("Calisphere API error: " . $res);
+    } else {
+      curl_close($curl);
+      $json = json_decode($res, true);
+      $index = 0;
+
+      foreach ($json["response"]["docs"] as $doc) {
+        $link = $doc["url_item"];
+        $title = $doc["title_ss"][0];
+        $results[] = [
+          'title' => $title,
+          'url' => $link,
+        ];
+
+        if ($index++ > 4) {
+          break;
+        }
+      }
+    }
 
     $response = [
       'query' => $this->query,
       'source' => 'Calisphere',
       'results' => $results,
-      'total' => 4,
-      'all_results_url' => 'https://calisphere.org/search/?q=' . urlencode($this->query)
+      'total' => $json['response']['numFound'],
+      'all_results_url' => "https://calisphere.org/search/?q=" . urlencode($this->query)
     ];
 
     return $response;
