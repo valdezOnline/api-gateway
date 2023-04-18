9090b28b033bede6be14a41cf9961c3d98cdfdc1
Scott Metoyer <scott.metoyer@gmail.com>
2023-04-18 06:33:59 -0700

    Tweaks to response JSON

--- app/Search/Sources/Calisphere.php
+++ app/Search/Sources/Calisphere.php
@@ -20,32 +20,35 @@ class Calisphere implements SearchSourceInterface
       'title' => 'Calisphere',
       'url' => 'https://calisphere.org/search/?q=' . urlencode($this->query),
       'description' => 'Calisphere is a gateway to digital collections from California\'s great libraries, archives, and museums. Discover over 1,000,000 images, texts, and recordings—and counting.',
-      'image' => 'https://calisphere.org/images/logo.png',
-      'all_results_url' => 'https://calisphere.org/search/?q=' . urlencode($this->query)
+      'image' => 'https://calisphere.org/images/logo.png'
     ];
     $results[] = [
       'title' => 'Calisphere 2',
       'url' => 'https://calisphere.org/search/?q=' . urlencode($this->query),
       'description' => 'Calisphere is a gateway to digital collections from California\'s great libraries, archives, and museums. Discover over 1,000,000 images, texts, and recordings—and counting.',
       'image' => 'https://calisphere.org/images/logo.png',
-      'all_results_url' => 'https://calisphere.org/search/?q=' . urlencode($this->query)
     ];
     $results[] = [
       'title' => 'Calisphere 34',
       'url' => 'https://calisphere.org/search/?q=' . urlencode($this->query),
       'description' => 'Calisphere is a gateway to digital collections from California\'s great libraries, archives, and museums. Discover over 1,000,000 images, texts, and recordings—and counting.',
       'image' => 'https://calisphere.org/images/logo.png',
-      'all_results_url' => 'https://calisphere.org/search/?q=' . urlencode($this->query)
     ];
     $results[] = [
       'title' => 'Calisphere 55',
       'url' => 'https://calisphere.org/search/?q=' . urlencode($this->query),
       'description' => 'Calisphere is a gateway to digital collections from California\'s great libraries, archives, and museums. Discover over 1,000,000 images, texts, and recordings—and counting.',
       'image' => 'https://calisphere.org/images/logo.png',
-      'all_results_url' => 'https://calisphere.org/search/?q=' . urlencode($this->query)
     ];
 
-    $response = ['query' => $this->query, 'source' => 'Calisphere', 'results' => $results, 'total' => 4];
+    $response = [
+      'query' => $this->query,
+      'source' => 'Calisphere',
+      'results' => $results,
+      'total' => 4,
+      'all_results_ur' => 'https://calisphere.org/search/?q=' . urlencode($this->query)
+    ];
+
     return $response;
   }
 }
--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -3,15 +3,17 @@
     query: new URLSearchParams(location.search).get('q'),
     results: [],
     count: 0,
+    all_results_url: ''
   }" x-init=" fetch('https://library-public-api-f7ju7.ondigitalocean.app/api/search?q=' + query + '&s=' + source)
   .then(res => res.json())
   .then(res => {
     results = res.results;
     count = res.total;
+    all_results_url = res.all_results_url;
   })">
   <div class="search-source-title">
     <div x-text="source">&nbsp</div>
-    <a class="all-results-link" :href="result.all_results_url">See all <span x-text="result.count">&nbsp;</span> results</a>
+    <a class="all-results-link" :href="all_results_url">See all <span x-text="count">&nbsp;</span> results</a>
   </div>
   <template x-for="result in results">
     <a class="search-link" x-text="result.title" :href="result.url">&nbsp;</a>
