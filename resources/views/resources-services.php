eacc37e8da7fee7d1708614b51b740b19241cace
Scott Metoyer <scott.metoyer@gmail.com>
2023-04-20 11:19:48 -0700

    Style updates

---
+++ resources/views/resources-services.php
@@ -0,0 +1,34 @@
+<div class="flex">
+  <div class="layout-col">
+    <h3><a href="https://searchworks.stanford.edu">Catalog</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="https://searchworks.stanford.edu/articles">Articles+</a></h3>
+    <p>Find books, media, journals, and more in Catalog; journal articles in Articles+.</p>
+
+    <h3><a href="https://searchworks.stanford.edu">Catalog</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="https://searchworks.stanford.edu/articles">Articles+</a></h3>
+    <p>Find books, media, journals, and more in Catalog; journal articles in Articles+.</p>
+  </div>
+
+  <div class="layout-col">
+    <h3><a href="https://searchworks.stanford.edu">Catalog</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="https://searchworks.stanford.edu/articles">Articles+</a></h3>
+    <p>Find books, media, journals, and more in Catalog; journal articles in Articles+.</p>
+
+    <h3><a href="https://searchworks.stanford.edu">Catalog</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="https://searchworks.stanford.edu/articles">Articles+</a></h3>
+    <p>Find books, media, journals, and more in Catalog; journal articles in Articles+.</p>
+  </div>
+
+  <div class="layout-col">
+    <h3><a href="https://searchworks.stanford.edu">Catalog</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="https://searchworks.stanford.edu/articles">Articles+</a></h3>
+    <p>Find books, media, journals, and more in Catalog; journal articles in Articles+.</p>
+
+    <h3><a href="https://searchworks.stanford.edu">Catalog</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="https://searchworks.stanford.edu/articles">Articles+</a></h3>
+    <p>Find books, media, journals, and more in Catalog; journal articles in Articles+.</p>
+  </div>
+
+  <div class="layout-col">
+    <h3><a href="https://searchworks.stanford.edu">Catalog</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="https://searchworks.stanford.edu/articles">Articles+</a></h3>
+    <p>Find books, media, journals, and more in Catalog; journal articles in Articles+.</p>
+
+    <h3><a href="https://searchworks.stanford.edu">Catalog</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="https://searchworks.stanford.edu/articles">Articles+</a></h3>
+    <p>Find books, media, journals, and more in Catalog; journal articles in Articles+.</p>
+  </div>
+
+</div>
