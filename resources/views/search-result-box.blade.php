3437a48dd101fdea65be9a7990f508131cf11f9e
Scott Metoyer <scott.metoyer@gmail.com>
2023-04-24 14:54:50 -0700

    Template updates

--- resources/views/search-result-box.blade.php
+++ resources/views/search-result-box.blade.php
@@ -177,3 +177,90 @@ fetch('https://library-public-api-f7ju7.ondigitalocean.app/api/search?q=' + quer
   </div>
 </div>
 </div>
+
+<!-- Databases -->
+<div class="search-block" x-data="{
+  source: 'Databases',
+  query: new URLSearchParams(location.search).get('q'),
+  results: [],
+  count: 0,
+  all_results_url: '',
+  loading: true,
+  error: ''
+}" x-init="
+fetch('https://library-public-api-f7ju7.ondigitalocean.app/api/search?q=' + query + '&s=' + source)
+.then(res => res.json())
+.then(res => {
+  results = res.results;
+  count = res.total;
+  all_results_url = res.all_results_url;
+  loading = false;
+  error = res.error;
+})">
+<div class="search-source-header">
+  <div class="source-title">Databases</div>
+  <a class="all-results-link" :href="all_results_url" x-show="count > 0">See all <span x-text="count">&nbsp;</span>
+    results</a>
+</div>
+<div class="search-results">
+  <p class="source-heading">Find the best library databases for your research.</p>
+  <template x-for="result in results">
+    <div class="search-result">
+      <a class="search-link" x-text="result.title" :href="result.url">&nbsp;</a>
+      <div class="result-details">
+        <span x-text="result.description">&nbsp;</span>
+      </div>
+    </div>
+  </template>
+
+  <div class="loading" x-show="loading">
+    Loading results...
+  </div>
+  <div class="search-error" x-show="error">
+    Error loading results: <span x-text="error"></span>
+  </div>
+</div>
+</div>
+
+<!-- LibraryWebsite -->
+<div class="search-block" x-data="{
+  source: 'LibraryWebsite',
+  query: new URLSearchParams(location.search).get('q'),
+  results: [],
+  count: 0,
+  all_results_url: '',
+  loading: true,
+  error: ''
+}" x-init="
+fetch('https://library-public-api-f7ju7.ondigitalocean.app/api/search?q=' + query + '&s=' + source)
+.then(res => res.json())
+.then(res => {
+  results = res.results;
+  count = res.total;
+  all_results_url = res.all_results_url;
+  loading = false;
+  error = res.error;
+})">
+<div class="search-source-header">
+  <div class="source-title">UCR Library Website</div>
+  <a class="all-results-link" :href="all_results_url" x-show="count > 0">See all <span x-text="count">&nbsp;</span>
+    results</a>
+</div>
+<div class="search-results">
+  <p class="source-heading">Search for results on the UCR Library Website.</p>
+  <template x-for="result in results">
+    <div class="search-result">
+      <a class="search-link" x-text="result.title" :href="result.url">&nbsp;</a>
+      <div class="result-details">
+        <span x-text="result.url">&nbsp;</span>
+    </div>
+  </template>
+
+  <div class="loading" x-show="loading">
+    Loading results...
+  </div>
+  <div class="search-error" x-show="error">
+    Error loading results: <span x-text="error"></span>
+  </div>
+</div>
+</div>
