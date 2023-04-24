98aebe0a842e5bbc920d3d1788f356e65414a40f
Scott Metoyer <scott.metoyer@gmail.com>
2023-04-24 14:32:57 -0700

    Pill updates

--- resources/views/search-result-box.blade.php
+++ resources/views/search-result-box.blade.php
@@ -132,3 +132,48 @@ fetch('https://library-public-api-f7ju7.ondigitalocean.app/api/search?q=' + quer
   </div>
 </div>
 </div>
+
+<!-- Course Reserves -->
+<div class="search-block" x-data="{
+  source: 'CourseReserves',
+  query: new URLSearchParams(location.search).get('q'),
+  results: [],
+  count: 0,
+  all_results_url: '',
+  loading: true,
+  error: ''
+}" x-init="
+fetch('https://library-public-api-f7ju7.ondigitalocean.app/api/search?q=' + query + '&s=' + source)
+.then(res => res.json())
+.then(res => {
+  results = res.results;
+  count = res.total;
+  all_results_url = res.all_results_url;
+  loading = false;
+  error = res.error;
+})">
+<div class="search-source-header">
+  <div class="source-title">Course Reserves</div>
+  <a class="all-results-link" :href="all_results_url" x-show="count > 0">See all <span x-text="count">&nbsp;</span>
+    results</a>
+</div>
+<div class="search-results">
+  <p class="source-heading">Library course reserves provide access to course readings and materials selected by instructors.</p>
+  <template x-for="result in results">
+    <div class="search-result">
+      <a class="search-link" x-text="result.title" :href="result.url">&nbsp;</a>
+      <div class="result-details">
+        <span class="pill" x-text="result.type">&nbsp;</span>&nbsp;<span class="pill blue">Source: <span x-text="result.source">&nbsp;</span></span>&nbsp;<span class="pill red">Course: <span x-text="result.crs">&nbsp;</span></span><br/>
+        <span x-text="result.description">&nbsp;</span>
+      </div>
+    </div>
+  </template>
+
+  <div class="loading" x-show="loading">
+    Loading results...
+  </div>
+  <div class="search-error" x-show="error">
+    Error loading results: <span x-text="error"></span>
+  </div>
+</div>
+</div>
