1461f296e2c16149cd4d032097fc2ed5fd0fddea
Scott Metoyer <scott.metoyer@gmail.com>
2023-08-09 13:52:15 -0700

    Added stubbed hours api call

--- app/Http/Controllers/Api/HoursController.php
+++ app/Http/Controllers/Api/HoursController.php
@@ -11,9 +11,34 @@ class HoursController extends Controller
     public function index(Request $request)
     {
         $location = $request->input('location');
+        $from = $request->input('from');
+        $to = $request->input('from');
 
-        $results = [];
+        // $results = call libapps api with $location, $from, $to
 
-        return response()->json($results);
+        // For now, mock the results to the library website done
+        $output = [
+            "locations" => []
+          ];
+
+        $output['locations'][] = [
+            'name' => 'rivera',
+            'open' => '2019-09-07T-15:50+00',
+            'close' => '2019-09-07T-15:50+00'
+        ];
+
+        $output['locations'][] = [
+            'name' => 'orbach',
+            'open' => '2019-09-07T-15:50+00',
+            'close' => '2019-09-07T-15:50+00 '
+        ];
+
+        $output['locations'][] = [
+            'name' => 'scua',
+            'open' => '2019-09-07T-15:50+00',
+            'close' => '2019-09-07T-15:50+00 '
+        ];
+
+        return response()->json($output);
     }
 }
