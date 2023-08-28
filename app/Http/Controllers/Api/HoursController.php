a0d90da6cfc105c02a93ef058f0f4cb1e2a91643
Scott Metoyer <scott.metoyer@gmail.com>
2023-08-28 15:12:03 -0700

    Build assets

--- app/Http/Controllers/Api/HoursController.php
+++ app/Http/Controllers/Api/HoursController.php
@@ -10,33 +10,24 @@ class HoursController extends Controller
 {
     public function index(Request $request)
     {
-        $location = $request->input('location');
-        $from = $request->input('from');
-        $to = $request->input('from');
-
-        // $results = call libapps api with $location, $from, $to
+        $start = $request->input('start');
 
         // For now, mock the results to the library website done
         $output = [
-            "locations" => []
+            "location" => []
           ];
 
-        $output['locations'][] = [
+        $output['location'][] = [
             'name' => 'rivera',
-            'open' => '2019-09-07T-15:50+00',
-            'close' => '2019-09-07T-15:50+00'
+
         ];
 
         $output['locations'][] = [
             'name' => 'orbach',
-            'open' => '2019-09-07T-15:50+00',
-            'close' => '2019-09-07T-15:50+00 '
         ];
 
         $output['locations'][] = [
             'name' => 'scua',
-            'open' => '2019-09-07T-15:50+00',
-            'close' => '2019-09-07T-15:50+00 '
         ];
 
         return response()->json($output);
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,20 @@
 import './bootstrap';
+
+function printStartAndEndOfCurrentWeek() {
+    const today = new Date();
+
+    // Get the first day of the current week (Sunday)
+    const firstDay = new Date(
+        today.setDate(today.getDate() - today.getDay()),
+    );
+
+    // Get the last day of the current week (Saturday)
+    const lastDay = new Date(
+        today.setDate(today.getDate() - today.getDay() + 6),
+    );
+
+    console.log(firstDay);
+    console.log(lastDay);
+}
+
+printStartAndEndOfCurrentWeek();
