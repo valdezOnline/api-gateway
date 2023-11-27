c879f40112ae89614fd70f462669b6cea5b7aad7
Scott Metoyer <scott.metoyer@gmail.com>
2023-11-26 16:44:34 -0800

    Merged in date buttons

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -6,7 +6,7 @@
 
     <title>Test</title>
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css'])
 </head>
 <body class="antialiased" onload="updateDateExceptions();">
 <button id="previous">Previous</button>
@@ -80,6 +80,50 @@
     </tr>
     </tbody>
 </table>
+<script type="text/javascript">
+    var selectedDate = new Date();
+
+    function fillCalendarDays() {
+        const today = new Date(selectedDate);
+        const days = [];
+        const names = ['SUN', 'MON', 'TUES', 'WEDS', 'THURS', 'FRI', 'SAT'];
+
+        // Get the days of the requested week
+        for (var i = 0; i < 7; i++) {
+            days.push(new Date(today.setDate(today.getDate() - today.getDay() + i)));
+            var xpath = "//td[text()[contains(.,'" + names[i] + "')]]";
+            var result = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+
+            for (var j = 0; j < result.snapshotLength; j++) {
+                var node = result.snapshotItem(j);
+                node.innerHTML = names[i] + " " + days[i].getDate();
+            }
+        }
+
+        try
+        {
+            document.getElementById('start').innerText = days[0].toLocaleDateString();
+            document.getElementById('end').innerText = days[6].toLocaleDateString();
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    function nextWeek() {
+        selectedDate.setDate(selectedDate.getDate() + 7);
+        fillCalendarDays();
+    }
+
+    function previousWeek() {
+        selectedDate.setDate(selectedDate.getDate() - 7);
+        fillCalendarDays();
+    }
+
+    fillCalendarDays();
+
+    document.querySelector('#next').addEventListener('click', nextWeek)
+    document.querySelector('#previous').addEventListener('click', previousWeek)
+</script>
 <script type="text/javascript" defer>
     // Save the old date values so we can rewrite them as needed
     var tables = document.querySelectorAll('table');
@@ -135,7 +179,7 @@
         // Find any exceptions for this location
         exceptions.forEach(function(exception) {
             dateIndex = new Date(start);
-            
+
             if (exception.location == 0 || locations[exception.location] == loc ) {
                 // Loop through the row cells and set exceptions for any days as needed
                 exceptionStart = new Date(exception.started_at);
