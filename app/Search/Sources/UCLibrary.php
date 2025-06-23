b640f28569dc335568c4ce5f391bf6f2e45377c1
Scott Metoyer <scott.metoyer@gmail.com>
2025-06-23 16:26:45 -0700

    Fixed the content to show description

--- app/Search/Sources/UCLibrary.php
+++ app/Search/Sources/UCLibrary.php
@@ -42,11 +42,12 @@ class UCLibrary implements SearchSourceInterface
     $index = 0;
 
     foreach ($json['docs'] as $element) {
+        ray($element);
       $docId = $element['pnx']['control']['recordid'][0] ?? '';
       $title = $element['pnx']['display']['title'][0] ?? '';
       $source = $element['pnx']['display']['source'][0] ?? '';
       $type = $element['pnx']['display']['type'][0] ?? '';
-      $contents = $element['pnx']['display']['contents'][0] ?? '';
+      $contents = $element['pnx']['display']['description'][0] ?? '';
       $link = 'https://search.library.ucr.edu/discovery/fulldisplay?docid=' . $docId . '&context=PC&vid=01CDL_RIV_INST:UCR&search_scope=Everything&lang=en';
 
       $results[] = [
