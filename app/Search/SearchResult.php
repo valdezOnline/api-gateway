e27c30cc54719357f67c23581e8376bec93082ec
Scott Metoyer <scott.metoyer@gmail.com>
2025-06-24 20:50:22 -0700

    Stripped tags from the search results content

--- app/Search/SearchResult.php
+++ app/Search/SearchResult.php
@@ -53,6 +53,7 @@ class SearchResult
 
       foreach ($this->results as $result) {
           $result['url'] = htmlspecialchars($result['url']);
+          $result['contents'] = strip_tags($result['contents']);
         $output .= "
 <item>
 <title>{$result['title']}</title>
